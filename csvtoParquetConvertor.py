5fd47092d0ae74c34636dc65a5b734fae5bf7556
Chezhian Ravikumar <androidgames6786@gmail.com>
2019-03-01 15:58:52 +0530

    Update csvtoParquetConvertor.py

--- csvtoParquetConvertor.py
+++ csvtoParquetConvertor.py
@@ -1,52 +1,42 @@
-import datetime
 import os
-import uuid as uid
 import pandas as pd
-import shutil
-import numpy as np
+import dateutil.parser
+import datetime
+
+import socket
+import binascii
+
+print(os.getcwd())
+print("\n")
+
+dateparser= lambda dt: int(datetime.datetime.strptime(dt, "%m/%d/%Y %I:%M:%S %p").timestamp() )
+
+hexval =binascii.hexlify(socket.inet_aton('0.0.0.0')).upper()
+
+loaded_df=pd.read_csv(filepath_or_buffer="C:/Users/chezhian_ravikumar/Documents/Python Scripts/Chezhian/test/20181225.csv",sep=",") .fillna(value=hexval)
+
+loaded_df.company_number=loaded_df.company_number.astype('int')
+loaded_df.device_number=loaded_df.device_number.astype('int')
+loaded_df.Start_time=  (pd.to_datetime(loaded_df.Start_time)).astype('int64')//1e9
+loaded_df.End_Time=  loaded_df.Start_time #(pd.to_datetime(loaded_df.End_Time)).astype('int64')//1e9
+
+ 
+
+loaded_df.columns=["company_number","device_number","DeviceNumber","log_line_count","start_time","end_time"]
+ 
+
+#loaded_df.start_time=dateutil.parser.parse(loaded_df.start_time).timestamp()
+ 
+
+print(loaded_df.dtypes)
 
 
-## Check if directory exists and create if needed
-def CheckandCreateFolders(ipfilename,otfolder,otfile):
-    strpartion = datetime.datetime.strptime(ipfilename[:8],"%Y%m%d").strftime("%Y-%m-%d")
-    outfolder = str.format(otfolder,strpartion)
-    outfile=str.format(otfile,uid.uuid4())
-    if os.path.exists(outfolder):
-        shutil.rmtree(outfolder)
-    os.makedirs(outfolder) 
-    return outfolder, outfile
+converted= loaded_df.to_parquet("C:/Users/chezhian_ravikumar/Documents/Python Scripts/Chezhian/test/Converted.parquet" )
+print(loaded_df.head())
+print(loaded_df.columns.values)
+print("conversion Completed")
+print("\n")
 
-# convert file and place in given folder
-def processfile(ipfolder,ipfilename,otfolder,otfile):
-    ipfilepath=str.format(ipfolder+ipfilename)
-    opfilepath=str.format(otfolder+otfile)
-    #csvinput=pd.read_csv(filepath_or_buffer=ipfilepath,sep=",",dtype={'company_number':int,'device_number':int,'source_binary_ip':str },parse_dates=["Start_time","End_Time"])
-    csvinput=pd.read_csv(filepath_or_buffer=ipfilepath,sep=",").fillna(value=' ')
-    
-    # Convert datetime to epoch
-    csvinput.Start_time=  (pd.to_datetime(csvinput.Start_time)).astype('int64')//1e9
-    csvinput.End_Time= csvinput.Start_time # (pd.to_datetime(csvinput.End_Time)).astype('int64')//1e9
-
-    #Rename Columns to match schema 
-    csvinput.columns=["company_number","device_number","source_binary_ip","log_line_count","start_time","end_time"]
-    csvinput.to_parquet(compression="snappy",fname=opfilepath)
-
-def main():
-    foldertolook="C:/Users/chezhian_ravikumar/Documents/Python Scripts/chezhian/2016/"
-    opfolder="C:/Users/chezhian_ravikumar/Documents/Python Scripts/chezhian/tblLogAggregrateSummary/partion={}/"
-    filename="part-00000-{}.c000.parquet"
-
-    files = next(os.walk(foldertolook))[2]
-
-    for f in files:
-        outfolder, outfile = CheckandCreateFolders(f,opfolder,filename)
-        processfile(foldertolook,f,outfolder,outfile)
-        #print(format("processed {} to {}",foldertolook+f,outfolder+outfile),end="\n")
-        print("processed {} to {} at {}".format(foldertolook+f,outfolder+outfile, datetime.datetime.now()),end="\n")
-        #print(f+ "\n" + f[:8]+ "\n" +outfolder+"\n"+outfile)
-
-    
-if __name__ == "__main__":
-    main()
-
-
+print("reading Parquet")
+par = pd.read_parquet("C:/Users/chezhian_ravikumar/Documents/Python Scripts/Chezhian/test/Converted.parquet")
+print(par.head())
